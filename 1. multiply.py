a6270ba7a574c6d304c7de8fbd0ab672cd8fa712
Cossich <cossichglauco@gmail.com>
2022-10-18 20:09:46 -0600

    1er ejercicio v0.2

--- 1. multiply.py
+++ 1. multiply.py
@@ -3,7 +3,6 @@
 #Tenemos que agregar las instrucciones de multiplicacion en el retorno.
 
 def multiply(a, b):#funcion "multiply" con parametros de entrada "(a,b)"
-    return a * b #sentencia de retorno o resultado, el cual puede ir en una linea arriba con una variable "resultado" , ej: "resultado = a * b" y retornamos la variable "resultado"
-
-multiply(12, 10) #invocacion por medio de la funcion, puede ser tambien por medio de un PRINT statment
+    return a * b #sentencia de retorno o resultado, el cual puede ir en una linea arriba con una variable "resultado" , ej: "resultado = a * b" y retornamos la variable "resultado".
+multiply(12, 10) #invocacion por medio de la funcion, puede ser tambien por medio de un PRINT statment.
 
