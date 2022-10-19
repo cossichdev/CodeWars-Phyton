271838abb804c304a2b3c19426e4ba591374d7ee
Cossich <cossichglauco@gmail.com>
2022-10-18 20:08:46 -0600

    1er ejercicio v0.2

--- 1. multiply.py
+++ 1. multiply.py
@@ -1,3 +1,7 @@
+#Nos piden arreglar el bloque de codigo con el pedazo faltante, nos proporcionan una funcion y nos piden que los parametros deban ser multiplicados. 
+
+#Tenemos que agregar las instrucciones de multiplicacion en el retorno.
+
 def multiply(a, b):#funcion "multiply" con parametros de entrada "(a,b)"
     return a * b #sentencia de retorno o resultado, el cual puede ir en una linea arriba con una variable "resultado" , ej: "resultado = a * b" y retornamos la variable "resultado"
 
