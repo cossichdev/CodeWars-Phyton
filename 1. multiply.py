d09e5144bd2a1740f88c121c8c8bd201b7ddad12
Cossich <cossichglauco@gmail.com>
2022-10-18 20:00:17 -0600

    1er ejercicio

---
+++ 1. multiply.py
@@ -0,0 +1,5 @@
+def multiply(a, b):#funcion "multiply" con parametros de entrada "(a,b)"
+    return a * b #sentencia de retorno o resultado, el cual puede ir en una linea arriba con una variable "resultado" , ej: "resultado = a * b" y retornamos la variable "resultado"
+
+multiply(12, 10) #invocacion por medio de la funcion, puede ser tambien por medio de un PRINT statment
+
